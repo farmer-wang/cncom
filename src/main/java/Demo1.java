b333f3c2c18d0b5c891221d6e0b206d8112f31c2
farmer-wang <1911497224@qq.com>
2020-12-28 14:50:53 +0800

    我的第一次commit提交

---
+++ src/main/java/Demo1.java
@@ -0,0 +1,14 @@
+/**
+ * @program: cncom
+ * @classname: Demo1
+ * @description: GitDemo
+ * @author: 王志春
+ * @date: 2020-12-28 14:45
+ **/
+public class Demo1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("helloworld");
+    }
+}
